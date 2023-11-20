25ff73d0288a0d9dab8408ba82e1580d88e93f1c
Marcos <marcosmaiksilva@gmail.com>
2023-11-19 22:30:47 -0300

    add main.css

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
     <>
       <NavBar/>
 
-      <main className=" relative justify-center items-center h-screen  flex">
-        <div>
-          <h1 className="title-pm">Estude <span>agora</span> na melhor plataforma de Tecnologia</h1>
+      <main className=" relative h-screen">
+        <div className="content absolute top-1/4 left-1/4 m-auto">
+          <h1 className="content__title cursor__animation title-pm">Estude <span>agora</span> na melhor plataforma de Tecnologia</h1>
         </div>
       </main>
     </>
