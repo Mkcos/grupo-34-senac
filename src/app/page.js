b937f2c5a53715c67cda6a412ea07cec1905db04
Marcos <marcosmaiksilva@gmail.com>
2023-11-19 21:38:42 -0300

    add animation nav

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,11 @@ import NavBar from "@/components/NavBar"
 export default function Home() {
   return (
     <>
-      <NavBar />
-      <main>
+      <NavBar/>
+
+      <main className=" relative justify-center items-center h-screen  flex">
         <div>
-          <h1>Estude <span>agora</span> na melhor plataforma de Tecnologia</h1>
+          <h1 className="title-pm">Estude <span>agora</span> na melhor plataforma de Tecnologia</h1>
         </div>
       </main>
     </>
